509ad6131ffede4963a66e54a8ace8768b6e7bda
hujintao <1145886822@qq.com>
2020-01-03 09:39:48 +0800

    Initial commit

---
+++ app/src/main/java/com/bawei/week2_1/base/BaseActivity.java
@@ -0,0 +1,9 @@
+package com.bawei.week2_1.base;
+
+/**
+ * 功能:  页面
+ * 作者:  胡锦涛
+ * 时间:  2020/1/3 0003 上午 9:39
+ */
+public class BaseActivity {
+}
---
+++ app/src/main/java/com/bawei/week2_1/model/MainModel.java
@@ -0,0 +1,9 @@
+package com.bawei.week2_1.model;
+
+/**
+ * 功能:  页面
+ * 作者:  胡锦涛
+ * 时间:  2020/1/3 0003 上午 9:38
+ */
+public class MainModel {
+}
